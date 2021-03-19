59b3ab50e26cdaeec8d772eee3d2f9809db3f8d8
Mike Simms <msimms01@gmail.com>
2021-03-19 18:10:08 -0400

    Now using JSON to pass data back to JavaScript.

--- interval-analyzer/src/lib.rs
+++ interval-analyzer/src/lib.rs
@@ -1,36 +1,20 @@
 // Copyright (c) 2021 Michael J. Simms. All rights reserved.
 
 extern crate gpx;
+extern crate serde;
+extern crate serde_json;
 
 mod utils;
 mod location_analyzer;
 
 use wasm_bindgen::prelude::*;
-
 use std::io::BufReader;
-use gpx::read;
-use gpx::Gpx;
-use gpx::errors::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-pub struct AnalysisReport {
-    pub start_time_ms: f64,
-    pub end_time_ms: f64,
-    pub total_distance: f64,
-    pub total_vertical: f64,
-    pub avg_speed: f64,
-    pub best_1k: f64,
-    pub best_mile: f64,
-    pub best_5k: f64,
-    pub best_10k: f64,
-    pub best_15k: f64,
-}
 
 #[wasm_bindgen]
 extern {
@@ -43,10 +27,11 @@ pub fn greet() {
 }
 
 #[wasm_bindgen]
-pub fn analyze_gpx(s: &str) -> AnalysisReport {
-    let mut analysis_report = AnalysisReport{ start_time_ms: 0.0, end_time_ms: 0.0, total_distance: 0.0, total_vertical: 0.0, avg_speed: 0.0, best_1k:0.0, best_mile: 0.0, best_5k: 0.0, best_10k: 0.0, best_15k: 0.0 };
+pub fn analyze_gpx(s: &str) -> String {
+    let mut analysis_report_str = String::new();
+
     let data = BufReader::new(s.as_bytes());
-    let res: Result<Gpx> = read(data);
+    let res = gpx::read(data);
 
     match res {
         Ok(gpx) => {
@@ -81,27 +66,34 @@ pub fn analyze_gpx(s: &str) -> AnalysisReport {
             analyzer.analyze();
 
             // Copy items to the final report.
-            analysis_report.start_time_ms = analyzer.start_time_ms as f64;
-            analysis_report.end_time_ms = analyzer.last_time_ms as f64;
-            analysis_report.total_distance = analyzer.total_distance;
-            analysis_report.total_vertical = analyzer.total_vertical;
-            analysis_report.avg_speed = analyzer.avg_speed;
-            analysis_report.best_1k = analyzer.get_best_time(location_analyzer::BEST_1K) as f64;
-            analysis_report.best_mile = analyzer.get_best_time(location_analyzer::BEST_MILE) as f64;
-            analysis_report.best_5k = analyzer.get_best_time(location_analyzer::BEST_5K) as f64;
-            analysis_report.best_10k = analyzer.get_best_time(location_analyzer::BEST_10K) as f64;
-            analysis_report.best_15k = analyzer.get_best_time(location_analyzer::BEST_15K) as f64;
+            analysis_report_str = serde_json::json!({
+                "Start Time (ms)": analyzer.start_time_ms,
+                "End Time (ms)": analyzer.last_time_ms,
+                "Elapsed Time": (analyzer.last_time_ms - analyzer.start_time_ms) / 1000,
+                "Total Distance": analyzer.total_distance,
+                "Total Vertical Distance": analyzer.total_vertical,
+                "Average Speed": analyzer.avg_speed,
+                "Best 1K": analyzer.get_best_time(location_analyzer::BEST_1K),
+                "Best Mile": analyzer.get_best_time(location_analyzer::BEST_MILE),
+                "Best 5K": analyzer.get_best_time(location_analyzer::BEST_5K),
+                "Best 10K": analyzer.get_best_time(location_analyzer::BEST_10K),
+                "Best 15K": analyzer.get_best_time(location_analyzer::BEST_15K),
+                "Best Half Marathon": analyzer.get_best_time(location_analyzer::BEST_HALF_MARATHON),
+                "Best Marathon": analyzer.get_best_time(location_analyzer::BEST_MARATHON),
+                "Mile Splits": analyzer.mile_splits,
+                "KM Splits": analyzer.km_splits
+            }).to_string();
         }
         Err(_e) => {
             alert("Error parsing GPX file.");
         }
     }
 
-    analysis_report
+    analysis_report_str
 }
 
 #[wasm_bindgen]
-pub fn analyze_tcx(_s: &str) -> AnalysisReport {
-    let analysis_report = AnalysisReport{ start_time_ms: 0.0, end_time_ms: 0.0, total_distance: 0.0, total_vertical: 0.0, avg_speed: 0.0, best_1k:0.0, best_mile: 0.0, best_5k: 0.0, best_10k: 0.0, best_15k: 0.0 };
-    analysis_report
+pub fn analyze_tcx(_s: &str) -> String {
+    let analysis_report_str = String::new();
+    analysis_report_str
 }
