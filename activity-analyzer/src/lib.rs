bf564cfde71bcb95766687324660835230c27e70
Mike Simms <msimms01@gmail.com>
2021-03-24 16:43:44 -0400

    Added front end code for loading tcx files.

--- activity-analyzer/src/lib.rs
+++ activity-analyzer/src/lib.rs
@@ -3,6 +3,7 @@
 extern crate gpx;
 extern crate serde;
 extern crate serde_json;
+extern crate tcx;
 
 mod utils;
 mod location_analyzer;
@@ -25,6 +26,30 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Copyright (c) 2021 Michael J. Simms. All rights reserved.");
+}
+
+fn make_final_report(analyzer: &location_analyzer::LocationAnalyzer) -> String {
+    let analysis_report_str = serde_json::json!({
+        "Start Time (ms)": analyzer.start_time_ms,
+        "End Time (ms)": analyzer.last_time_ms,
+        "Elapsed Time": (analyzer.last_time_ms - analyzer.start_time_ms) / 1000,
+        "Total Distance": analyzer.total_distance,
+        "Total Vertical Distance": analyzer.total_vertical,
+        "Average Speed": analyzer.avg_speed,
+        "Best 1K": analyzer.get_best_time(location_analyzer::BEST_1K),
+        "Best Mile": analyzer.get_best_time(location_analyzer::BEST_MILE),
+        "Best 5K": analyzer.get_best_time(location_analyzer::BEST_5K),
+        "Best 10K": analyzer.get_best_time(location_analyzer::BEST_10K),
+        "Best 15K": analyzer.get_best_time(location_analyzer::BEST_15K),
+        "Best Half Marathon": analyzer.get_best_time(location_analyzer::BEST_HALF_MARATHON),
+        "Best Marathon": analyzer.get_best_time(location_analyzer::BEST_MARATHON),
+        "Mile Splits": analyzer.mile_splits,
+        "KM Splits": analyzer.km_splits,
+        "Times": analyzer.speed_times,
+        "Speeds": analyzer.speed_graph
+    }).to_string();
+
+    analysis_report_str
 }
 
 #[wasm_bindgen]
@@ -67,25 +92,7 @@ pub fn analyze_gpx(s: &str) -> String {
             analyzer.analyze();
 
             // Copy items to the final report.
-            analysis_report_str = serde_json::json!({
-                "Start Time (ms)": analyzer.start_time_ms,
-                "End Time (ms)": analyzer.last_time_ms,
-                "Elapsed Time": (analyzer.last_time_ms - analyzer.start_time_ms) / 1000,
-                "Total Distance": analyzer.total_distance,
-                "Total Vertical Distance": analyzer.total_vertical,
-                "Average Speed": analyzer.avg_speed,
-                "Best 1K": analyzer.get_best_time(location_analyzer::BEST_1K),
-                "Best Mile": analyzer.get_best_time(location_analyzer::BEST_MILE),
-                "Best 5K": analyzer.get_best_time(location_analyzer::BEST_5K),
-                "Best 10K": analyzer.get_best_time(location_analyzer::BEST_10K),
-                "Best 15K": analyzer.get_best_time(location_analyzer::BEST_15K),
-                "Best Half Marathon": analyzer.get_best_time(location_analyzer::BEST_HALF_MARATHON),
-                "Best Marathon": analyzer.get_best_time(location_analyzer::BEST_MARATHON),
-                "Mile Splits": analyzer.mile_splits,
-                "KM Splits": analyzer.km_splits,
-                "Times": analyzer.speed_times,
-                "Speeds": analyzer.speed_graph
-            }).to_string();
+            analysis_report_str = make_final_report(&analyzer);
         }
         Err(_e) => {
             alert("Error parsing GPX file.");
@@ -96,7 +103,32 @@ pub fn analyze_gpx(s: &str) -> String {
 }
 
 #[wasm_bindgen]
-pub fn analyze_tcx(_s: &str) -> String {
-    let analysis_report_str = String::new();
+pub fn analyze_tcx(s: &str) -> String {
+    let mut data = BufReader::new(s.as_bytes());
+    let res = tcx::read(&mut data);
+    let mut analyzer = location_analyzer::LocationAnalyzer::new();
+    let activities = res.activities.unwrap();
+
+    for activity in activities.activities {
+        for lap in activity.laps {
+            for track in lap.tracks {
+                for trackpoint in track.trackpoints {
+                    let time = trackpoint.time.timestamp();
+                    let position = trackpoint.position.unwrap();
+                    let altitude = trackpoint.altitude_meters.unwrap();
+
+                    analyzer.append_location(time as u64, position.latitude, position.longitude, altitude);
+                    analyzer.update_speeds();
+                }
+            }
+        }
+    }
+
+    // For calculations that only make sense once all the points have been added.
+    analyzer.analyze();
+
+    // Copy items to the final report.
+    let analysis_report_str = make_final_report(&analyzer);
+
     analysis_report_str
 }
