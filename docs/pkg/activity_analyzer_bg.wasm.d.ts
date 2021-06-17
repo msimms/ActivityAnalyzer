3223f6524e5892e249db11bddf5b38cd2b3c81a3
Mike Simms <msimms01@gmail.com>
2021-06-17 14:00:10 -0400

    Added release files.

---
+++ docs/pkg/activity_analyzer_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(): void;
+export function analyze_gpx(a: number, b: number, c: number): void;
+export function analyze_tcx(a: number, b: number, c: number): void;
+export function analyze_fit(a: number, b: number, c: number): void;
+export function export_data(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
